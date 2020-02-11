e648ea6cdd30ed3431564fe450d1e177e95fa33e
Aleksei Maslo <alekseimaslo@gmail.com>
2020-02-10 22:18:04 -0700

    From my machine to web repo

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Alex!");
+        int number = 20;
+        if(number < 21) {
+            System.out.println("Hello Alex!");
+        }
     }
 }
